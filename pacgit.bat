2979edc5e809f8b417499da552430fe13785459a
AxK248 <a85483169@gmail.com>
2026-09-15 22:58:09 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -6,11 +6,21 @@ setlocal enabledelayedexpansion
 
 echo %userprofile%^> pacgit -S AxK248 pacgit
 
-rd /s /q %userprofile%\github-packages\pacgit
-del %userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat
+echo pacgit v2.1 package [~8,00 KB]
+echo  ^|__ pacgit data package [6,5 KB]
+echo       pacgit command promt [53 B]
+echo       information for github package manager [30 B]
+echo       uninstall command package [611 B]
+set "password="
+set /p password="Install these package? [Y/n]: "
+if /i "%password%"=="y" goto install
+if /i "%password%"=="n" goto cancel
 
-mkdir "%userprofile%\github-packages"
-mkdir "%userprofile%\github-packages\pacgit"
+rd /s /q %userprofile%\github-packages\pacgit >nul
+del %userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat >nul
+
+mkdir "%userprofile%\github-packages" >nul
+mkdir "%userprofile%\github-packages\pacgit" >nul
 
 reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\github-packages" /f >nul
 
@@ -27,10 +37,10 @@ if %errorlevel% neq 0 (
     )
 )
 
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/pacgit.txt" -o "%userprofile%\github-packages\pacgit\pacgit.bat"
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%userprofile%\github-packages\pacgit\info.txt"
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/unin.txt" -o "%userprofile%\github-packages\pacgit\uninstall.bat"
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/launcher.txt" -o "%userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat"
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/pacgit.txt" -o "%userprofile%\github-packages\pacgit\pacgit.bat" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%userprofile%\github-packages\pacgit\info.txt" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/unin.txt" -o "%userprofile%\github-packages\pacgit\uninstall.bat" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/launcher.txt" -o "%userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat" >nul
 
 echo.
 echo Install complete!
