0d87aeb4fc9786f6a471216d666d41f29ab646eb
AxK248 <a85483169@gmail.com>
2026-09-15 14:39:39 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -3,27 +3,35 @@
 :: pacwin installer for windows
 :: Start a installer
 setlocal enabledelayedexpansion
+
 echo %userprofile%^> pacwin -S axk248 pacwin
-rd /s /q %userprofile%\.github_packages
-mkdir %userprofile%\.github_packages
 
 :: 1. Search current PATH in HKCU
-:: We're looking to see if our folder is already there (We are looking specifically for the text %USERPROFILE%\.github-packages and %git-package% with %pacgit%)
-for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v PATH 2^>nul') do (
-    set "user_path=%%b"
-)
 
-echo !user_path! | findstr /i /c:"%git-package%;%git-package%\pacgit" >nul
+:: We're looking to see if our folder is already there (We are looking specifically for the text %USERPROFILE%\.github-packages and %git-package% with %pacgit%)
+
+set "old_path="
+for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v PATH 2^>nul') do set "old_path=%%b"
+
+echo !old_path! | findstr /i /c:".github-packages\pacgit" >nul
 
 if %errorlevel% neq 0 (
-    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "!user_path!;%%git-package%%;%%git-package%%\pacgit" /f >nul
+    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "!old_path!;^%git-package^%\;^%git-package^%\pacgit" /f >nul
 )
-reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\.github-packages" /f
-mkdir %userprofile%\.github_packages\pacgit
+
+
+reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\.github-packages" /f >nul
+reg add "HKCU\Environment" /v pacgit /t REG_EXPAND_SZ /d "%%git-package%%\pacgit" /f >nul
+
+mkdir /s /q "%userprofile%\.github-packages" 2>nul
+rd /s /q "%userprofile%\.github-packages\pacgit" 2>nul
+mkdir "%userprofile%\.github-packages\pacgit" 2>nul
+
 curl -sL "https://raw.githubusercontent.com/axk248/pacgit/main/pacgit-dat.bat" -o %userprofile%\.github_packages\pacgit\pacgit.bat
 curl -sL "https://raw.githubusercontent.com/axk248/pacgit/main/info.txt" -o %userprofile%\.github_packages\pacgit\info.txt
-echo %userprofile%\.github_package\pacgit^> echo Install complate
+
+echo %%git-package%%\pacgit^> echo Install complate
 echo Install complate
 timeout /t -1
-exit /b
 @echo on
+exit /b
