ba65fdd0ff6a9849d518c9cf451cfbc553f58d1d
AxK248 <a85483169@gmail.com>
2026-09-15 15:47:31 +0300

    Create uninstall.bat

---
+++ uninstall.bat
@@ -0,0 +1,22 @@
+:Uninstalling
+@echo off
+chcp 65001
+
+echo If you remove pacgit, all packages downloaded via it will also be removed.
+echo Package pacgit [~8,00 KB]
+echo Folder .github-packages [?]
+set "password="
+set /p password="Remove these package? [Y/n]: "
+if /i "password"="Y" goto delete
+if /i "password"="n" goto cancel
+
+:cacnel
+echo Cancellation...
+exit /b
+
+:delete
+rd /s /q %git-package%
+reg delete "HKCU\Environment" /v git-package /f
+echo Deleting is completed
+timeout /t -1
+exit /b
