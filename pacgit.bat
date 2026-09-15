f554c474c37464be5edeb8599fdb8c28dc11336b
AxK248 <a85483169@gmail.com>
2026-09-15 17:06:40 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -10,7 +10,6 @@ mkdir "%userprofile%\github-packages"
 mkdir "%userprofile%\github-packages\pacgit"
 
 reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\github-packages" /f >nul
-mklink /J %userprofile%\github-packages %appdata%\github-packages
 
 for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v PATH 2^>nul') do set "user_path=%%B"
 
@@ -28,6 +27,8 @@ if %errorlevel% neq 0 (
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/pacgit.txt" -o "%userprofile%\.github-packages\pacgit\pacgit.bat"
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%userprofile%\.github-packages\pacgit\info.txt"
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/unin.txt" -o "%userprofile%\.github-packages\pacgit\uninstall.bat"
+
+mklink /J %userprofile%\github-packages %appdata%\github-packages
 
 echo.
 echo Install complete!
