40c7960624a32b945cddaf9d85b9be0472fbbf8a
AxK248 <a85483169@gmail.com>
2026-09-15 14:12:43 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -8,14 +8,16 @@ mkdir %userprofile%/.github_packages
 
 :: 1. Search current PATH in HKCU
 :: We're looking to see if our folder is already there (We are looking specifically for the text %USERPROFILE%\.github-packages and %git-package% with %pacgit%)
-set "old_path="
 for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v PATH 2^>nul') do (
-    set "old_path=%%b"
+    set "user_path=%%b"
 )
-echo !old_path! | findstr /i /c:"%%git-package%%" >nul
+
+echo !user_path! | findstr /i /c:"%APPDATA%\CMDoverWIN" >nul
+
 if %errorlevel% neq 0 (
-    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%%old_path%%;%%git-package%%;%%pacgit%%" /f >nul
+    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "!user_path!;%%gitpackage%%;%%git-package%%\pacgit" /f >nul
 )
+
 reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\.github-packages" /f
 mkdir %userprofile%\.github_packages\pacgit
 curl -sL "https://raw.githubusercontent.com/axk248/pacgit/main/pacgit-dat.bat" -o %userprofile%\.github_packages\pacgit\pacgit.bat
