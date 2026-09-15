6a98879fef1132ca00af14b547da10012d81702e
AxK248 <a85483169@gmail.com>
2026-09-15 13:33:49 +0300

    Create pacgit.bat

---
+++ pacgit.bat
@@ -0,0 +1,28 @@
+:: Version: 1.0
+:: pacwin installer for windows
+:: Start a installer
+setlocal enabledelayedexpansion
+echo %userprofile%^> pacwin -S axk248 pacwin
+mkdir %userprofile%/.github_packages
+:: 1. Search current PATH in HKCU
+We're looking to see if our folder is already there (We are looking specifically for the text %USERPROFILE%\.github-packages and %git-package% with %pacgit%)
+echo !user_path! | findstr /i /c:"%%git-package%%" >nul
+
+:: 2. If the path doesn't exist, we create it.
+for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v PATH 2^>nul') do (
+    set "user_path=%%b"
+)
+echo !user_path! | findstr /i /c:"%git-package%;%pacgit%" >nul
+
+if %errorlevel% neq 0 (
+    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "!user_path!;%%git-package%%;%%pacgit%%" /f >nul
+)
+reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "^%USERPROFILE^%\.github-packages"
+mkdir %userprofile%\.github_packages\pacgit
+reg add "HKCU\Environment" /v pacgit /t REG_EXPAND_SZ /d "^%git-package^%\pacgit"
+curl -sL "https://raw.githubusercontent.com/axk248/pacgit/main/pacgit-dat.bat" -o %userprofile%\.github_packages\pacgit\pacgit.bat
+curl -sL "https://raw.githubusercontent.com/axk248/pacgit/main/info.txt" -o %userprofile%\.github_packages\pacgit\info.txt
+echo %git-package%\pacgit^> echo Install complate
+echo Install complate
+timeout /t -1
+exit /b
