b150d3f99902b266423661be6a51cf1cb390149e
AxK248 <a85483169@gmail.com>
2026-09-26 01:31:15 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -26,25 +26,10 @@ del %userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat >nul
 mkdir "%userprofile%\github-packages" >nul
 mkdir "%userprofile%\github-packages\pacgit" >nul
 
-reg add "HKCU\Environment" /v git-package /t REG_EXPAND_SZ /d "%%USERPROFILE%%\github-packages" /f >nul
-
-for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v PATH 2^>nul') do set "user_path=%%B"
-
-if "%user_path%"=="" set "user_path="
-
-echo !user_path! | findstr /i /c:"%%git-package%%" >nul
-if %errorlevel% neq 0 (
-    if defined user_path (
-        reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "!user_path!;%%git-package%%" /f >nul
-    ) else (
-        reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%%git-package%%" /f >nul
-    )
-)
-
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/pacgit.txt" -o "%userprofile%\github-packages\pacgit\pacgit.bat" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%userprofile%\github-packages\pacgit\pacgit.bat" >nul
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%userprofile%\github-packages\pacgit\info.txt" >nul
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/unin.txt" -o "%userprofile%\github-packages\pacgit\uninstall.bat" >nul
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/launcher.txt" -o "%userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%userprofile%\github-packages\pacgit\uninstall.bat" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat" >nul
 
 echo.
 echo Install complete!
