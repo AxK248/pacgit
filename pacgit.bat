746d1fd83b06d40263d5feae97385cc9a6c680b0
AxK248 <a85483169@gmail.com>
2026-09-26 02:25:54 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -1,5 +1,14 @@
 :: Version: 2.1
 :: pacwin installer for windows
+if "%~1" == "win" goto installer
+
+@echo off
+curl -sL "https://raw.github.com/AxK248/pacgit/main/pacgit.bat" -o "%TEMP%\install.bat" >nul
+call "%TEMP%\install.bat" win
+del "%TEMP%\install.bat" >nul
+exit /b
+
+:installer
 @echo off
 chcp 65001 >nul
 setlocal enabledelayedexpansion
@@ -20,19 +29,24 @@ echo Default Y... Press enter to continue
 timeout /t -1 >nul 
 
 :install
-rd /s /q %userprofile%\github-packages\pacgit >nul
-del %userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat >nul
+rd /s /q %userprofile%\github-packages\pacgit 2>nul
+del %userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat 2>nul
 
 mkdir "%userprofile%\github-packages" >nul
 mkdir "%userprofile%\github-packages\pacgit" >nul
+
+echo Downloading packages
 :: pacgit command promt
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%userprofile%\github-packages\pacgit\pacgit.bat" >nul
+echo pacgit command promt. Done.
 :: information
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%userprofile%\github-packages\pacgit\info.txt" >nul
 :: Uninstall command
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%userprofile%\github-packages\pacgit\uninstall.bat" >nul
+echo Creating uninstalling batch. Done.
 :: Shortcut for cmd
 curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%userprofile%\appdata\local\microsoft\windowsapps\pacgit.bat" >nul
+echo Creating shortcut for cmd. Done.
 
 echo.
 echo Install complete!
