268cf59cee9fed5e65ea7f9dd36943317093bbbe
AxK248 <a85483169@gmail.com>
2026-09-26 12:31:07 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -6,6 +6,7 @@ echo  ^|__ pacgit data package [7,24 KB]
 echo      ^| pacgit command promt package [6,59 KB]
 echo      ^| shortcut for cmd package [71 B]
 echo      ^| uninstall command package [592 B]
+echo      ^| gsudo package [?]
 set "password="
 set /p password="Install these package? [Y/n]: "
 if /i "%password%"=="y" goto install
@@ -15,27 +16,38 @@ echo Default Y... Press enter to continue
 timeout /t -1 >nul 
 
 :install
-rd /s /q %LocalAppData%\github-packages\pacgit 2>nul
-del %LocalAppData%\Microsoft\WindowsApps\pacgit.bat 2>nul
+rd /s /q "%ProgramData%\github-packages\pacgit" 2>nul
+del "%ProgramData%\github-packages\gsudo.exe" 2>nul
+del "%windir%\pacgit.bat" 2>nul
+del "%windir%\gsudo.exe" 2>nul
 
-mkdir "%LocalAppData%\Programs\github-packages" >nul
-mkdir "%LocalAppData%\Programs\github-packages\pacgit" >nul
+mkdir "%ProgramData%\github-packages" >nul
+mkdir "%ProgramData%\github-packages\pacgit" >nul
 
 echo Downloading packages
 :: pacgit command promt
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%LocalAppData%\Programs\github-packages\pacgit\pacgit.bat" >nul
-echo pacgit command promt. Done.
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%ProgramData%\github-packages\pacgit\pacgit.bat" >nul
+echo pacgit command promt — Done.
 :: information
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%LocalAppData%\Programs\github-packages\pacgit\info.txt" >nul
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%ProgramData%\github-packages\pacgit\info.txt" >nul
 :: Uninstall command
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%LocalAppData%\Programs\github-packages\pacgit\uninstall.bat" >nul
-echo Creating uninstalling batch. Done.
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%ProgramData%\github-packages\pacgit\uninstall.bat" >nul
+echo Creating uninstalling batch — Done.
 :: Shortcut for cmd
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%LocalAppData%\Microsoft\WindowsApps\pacgit.bat" >nul
-echo Creating shortcut for cmd. Done.
+curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%windir%\pacgit.bat" >nul
+echo Creating shortcut for cmd — Done.
+:: Downloading gsudo package
+curl -sL "https://github.com/gerardog/gsudo/releases/download/v2.6.1/gsudo.portable.zip" -o "%ProgramData%\github-packages\gsudo.zip"
+tar -xf "%ProgramData%\github-packages\gsudo.zip"
+del "%ProgramData%\github-packages\gsudo.zip"
+rmdir /s /q "%ProgramData%\git-packages\arm64"
+rmdir /s /q "%ProgramData%\git-packages\net46-AnyCpu"
+rmdir /s /q "%ProgramData%\git-packages\x86"
+move "%ProgramData%\git-packages\x64\gsudo.exe" "%windir%\gsudo.exe"
+rmdir /s /q "%ProgramData%\git-packages\x64"
+echo Downloading gsudo package — Done.
 
 echo.
 echo Install complete!
 timeout /t -1
 exit /b
-
