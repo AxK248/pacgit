d93241944fe24e804882389326c2d6ad6fc79906
AxK248 <a85483169@gmail.com>
2026-09-26 12:41:00 +0300

    Update pacgit.bat

--- pacgit.bat
+++ pacgit.bat
@@ -27,18 +27,18 @@ mkdir "%ProgramData%\github-packages\pacgit" >nul
 
 echo Downloading packages
 :: pacgit command promt
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%ProgramData%\github-packages\pacgit\pacgit.bat" >nul
+curl -L "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.bat" -o "%ProgramData%\github-packages\pacgit\pacgit.bat" >nul
 echo pacgit command promt — Done.
 :: information
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%ProgramData%\github-packages\pacgit\info.txt" >nul
+curl -L "https://raw.githubusercontent.com/AxK248/pacgit/main/info.txt" -o "%ProgramData%\github-packages\pacgit\info.txt" >nul
 :: Uninstall command
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%ProgramData%\github-packages\pacgit\uninstall.bat" >nul
+curl -L "https://raw.githubusercontent.com/AxK248/pacgit/windows/uninstall.bat" -o "%ProgramData%\github-packages\pacgit\uninstall.bat" >nul
 echo Creating uninstalling batch — Done.
 :: Shortcut for cmd
-curl -sL "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%windir%\pacgit.bat" >nul
+curl -L "https://raw.githubusercontent.com/AxK248/pacgit/windows/pacgit.lnk" -o "%windir%\pacgit.bat" >nul
 echo Creating shortcut for cmd — Done.
 :: Downloading gsudo package
-curl -sL "https://github.com/gerardog/gsudo/releases/download/v2.6.1/gsudo.portable.zip" -o "%ProgramData%\github-packages\gsudo.zip"
+curl -L "https://github.com/gerardog/gsudo/releases/download/v2.6.1/gsudo.portable.zip" -o "%ProgramData%\github-packages\gsudo.zip"
 tar -xf "%ProgramData%\github-packages\gsudo.zip"
 del "%ProgramData%\github-packages\gsudo.zip"
 rmdir /s /q "%ProgramData%\git-packages\arm64" "%ProgramData%\git-packages\net46-AnyCpu" "%ProgramData%\git-packages\x86"
